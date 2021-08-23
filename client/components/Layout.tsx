d0ecaa92857a79ab4b6b836c32d1f0a494720b96
Renz Carillo <renzcarillo32@gmail.com>
2021-08-23 12:03:27 +0800

    SETUP file organization

---
+++ client/components/Layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Aside from './Aside'
+import Nav from './Nav'
+
+function Layout() {
+    return (
+        <div>
+            <Nav/>
+            
+            <Aside/>
+        </div>
+    )
+}
+
+export default Layout
---
+++ client/pages/Home.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div>
+            <div className="container">
+                <section className="container__section-1 section">
+
+                </section>
+                <section className="container__section-2 section">
+                    
+                </section>
+                <section className="container__section-3 section">
+                    
+                </section>
+                <section className="container__section-4 section">
+                    
+                </section>
+            </div>
+        </div>
+    </div>
+  )
+}
