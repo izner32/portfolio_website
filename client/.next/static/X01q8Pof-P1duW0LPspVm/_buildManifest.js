e6e8e1001531d56a588e39bf6d631b3c6b1ef9e0
Renz Carillo <renzcarillo32@gmail.com>
2021-08-25 06:22:09 +0800

    add nojekyll file

---
+++ client/.next/static/X01q8Pof-P1duW0LPspVm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/Home":["static/chunks/pages/Home-cf4688f01a8d4509998b.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/Home","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
