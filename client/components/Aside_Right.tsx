fdcee2595f21d4105b37e820204dfb46d479d794
Renz Carillo <renzcarillo32@gmail.com>
2021-08-25 06:39:10 +0800

    edited info on right aside

--- client/components/Aside_Right.tsx
+++ client/components/Aside_Right.tsx
@@ -8,7 +8,7 @@ function Aside_Right() {
             <div className={styles.aside_right__container}>
                 <p className={styles.container__intro}>Hello, I am</p>
                 <h1 className={styles.container__name}>Renz <br/>Carillo</h1>
-                <p className={styles.container__info}>Im a 1st year computer science student from De La Salle University - Dasmariñas. Im mainly interested into the magics of data science. My main focus is into web development, im also an forthcoming full stack web developer.
+                <p className={styles.container__info}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, expedita voluptatem quisquam, assumenda ad totam sunt illo non unde amet rerum consequatur odio, aspernatur maxime. Repudiandae, necessitatibus assumenda. Quae, hic!
                 </p>
                 <div className={styles.container__skills}>
                     <Image className={styles.container__logo} src="/html.png" width={40} height={40} alt="html"></Image>
